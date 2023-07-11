0a8716defe8a01b38188fd04e440c684a348b6bc
ivaldo 'pedro' martins de souza oliveira <138531232+IvaldoPedroMartins@users.noreply.github.com>
2023-07-10 23:37:29 -0300

    Update codigo jogo adivinhação.php

--- codigo jogo adivinhação.php
+++ codigo jogo adivinhação.php
@@ -2,7 +2,6 @@
 /*sorteio do numero secreto*/
 $secreto= rand(1,30);
 echo "o numero secreto é $secreto \n\n";
-
 //tentativas
 $tentativas=4;
 while($tentativas>=0){
@@ -11,12 +10,10 @@ $escolha=15;
 if ($escolha==$secreto){
 echo "você acertou.";
 break;}
-
 elseif($escolha<$secreto)
 echo "você errou, tente novamente.\nDica: o seu valor esta abaixo do numero secreto.\n\n";
 elseif($escolha>$secreto)
 echo "você errou, tente novamente.\nDica: o seu valor esta a cima do numero secreto.\n\n";
-
 echo "resta: $tentativas tentativas.";
 $tentativas--;
 break;
